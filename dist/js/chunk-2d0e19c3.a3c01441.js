b0ef9706e2129272701c8f89a63205d2336229a2
YueZhao2019 <87960589+YueZhao2019@users.noreply.github.com>
2021-12-17 19:38:23 +0800

    fix: UI updated

--- dist/js/chunk-2d0e19c3.a3c01441.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e19c3"],{"7abe":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("HOME")])},c=[],u={name:"Home"},o=u,r=t("2877"),s=Object(r["a"])(o,a,c,!1,null,"969798a2",null);n["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0e19c3.a3c01441.js.map
